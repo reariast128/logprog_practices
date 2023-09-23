f5fa1cceb4a88ec7766b9d2d34ae1ce94c69859e
Ricardo Arias <reariast128@gmail.com>
2023-09-23 16:22:41 -0500

    Revert "Removido aquel archivo de nombre tan largo."
    This reverts commit a428a39a4e805cdd94605eec75694b2cd8b335f9.

---
+++ task 27-09/no se exactamente cual numero de ejercicio es, pero yo digo que es el 3.py
@@ -0,0 +1,15 @@
+def create_list(higher_number):
+    a = higher_number
+    list = []
+    if a == 1:
+        return list
+    else:
+
+
+
+
+user_number = int(input("Introduce un número entero: "))
+if user_number == 0:
+    print("Estoy programado para no hacer ndaa. No puedo ayudarte.")
+
+
