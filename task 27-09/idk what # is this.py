18953803741f4a9c85bdf21e7d8bb68b865f9d60
Ricardo Arias <reariast128@gmail.com>
2023-09-25 23:08:02 -0500

    Terminado este ejercicio.

--- task 27-09/idk what # is this.py
+++ task 27-09/idk what # is this.py
@@ -1,15 +1,16 @@
-def create_list(higher_number):
-    a = higher_number
-    list = []
-    if a == 1:
-        return list
-    else:
+def reversed_range_list(max_number):
+    return [x for x in reversed(range(1, max_number + 1))]
 
+lista = ['Alicia', 3]
+print(lista)
 
+print(f"La lista actual tiene los siguientes elementos: {lista}")
+user_number = int(input("Introduce un número: "))
+if user_number > 0:
+    list_from_user_number = reversed_range_list(user_number)
+    for number in list_from_user_number:
+        lista.append(number)
+    print(f"La nueva lista tiene los siguientes elementos: {lista}")
 
-
-user_number = int(input("Introduce un número entero: "))
-if user_number == 0:
-    print("Estoy programado para no hacer ndaa. No puedo ayudarte.")
-
-
+else:
+    print("Estoy programado para no hacer nada si el número es menor o igual a cero.")
